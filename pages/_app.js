9d8bf67bf912a9422a328105a0170bea273ba839
HittuDesai <hitarth.desai@mail.utoronto.ca>
2022-08-19 20:26:48 +0530

    Initial Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+	const darkTheme = createTheme({
+		palette: {
+			mode: "dark",
+		},
+	});
+
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default App;
